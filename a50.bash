0a9595179c3ffe78f14623b8b1041e003ab1c53c
Wang Qi <85624226@qq.com>
2020-02-26 14:10:16 +0800

    Update and rename a50 to a50.bash

--- a50.bash
+++ a50.bash
@@ -1,14 +1,14 @@
-    
+
 gdown --id 1QB1cw91ZMv2rzWSUYhtiQcHNzCWBPT3t
 unzip -o wq20200110.zip > /dev/null
 
-gdown --id 16E6C5WuNgZKaJSryls2Zen1ApdGcY7dW
+gdown --id 11Atbh78Y94A56teLSndwemWwbxApSg4t
 mv *.gz networks
 
 chmod a+x autogtp
 chmod a+x leelaz
 
-for i in $(seq 1 5)
+for i in $(seq 1 10)
 do 
 ./autogtp  -k ./train/ -m 10| grep minute
 sleep 23
